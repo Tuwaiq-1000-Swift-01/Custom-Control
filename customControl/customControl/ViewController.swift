67144360ffae736a665fd9e3210aa2980d6f02e6
Osa3a-Fallatah <91524745+Osa3a-Fallatah@users.noreply.github.com>
2022-01-16 21:18:46 +0300

    Add files via upload

---
+++ customControl/customControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  customControl
+//
+//  Created by Osama folta on 13/06/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
